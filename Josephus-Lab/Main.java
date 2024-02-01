16eb2cc547374cebedae0116a23bec07751e01d2
braindetrain <135712063+braindetrain@users.noreply.github.com>
2024-01-31 22:44:29 -0500

    Create Main.java

---
+++ Josephus-Lab/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    try{
+      Josephus.main(args);
+    }
+    catch(Exception e){
+      System.out.println("Something went wrong");
+    }
+  }
+}
