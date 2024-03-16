b4730c4e5faea9261c4a4cef5dd801c291024242
braindetrain <135712063+braindetrain@users.noreply.github.com>
2024-03-16 16:26:31 -0400

    Create Main.java

---
+++ SetsOfLetters_Lab/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    try{
+      SetsOfLetters.main(args);
+    }
+    catch(Exception e){
+      System.out.println("Something went wrong");
+    }
+  }
+}
