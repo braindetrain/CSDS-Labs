fcb0b96417de5f57cee5ff9ebebf7ab8116c487b
braindetrain <135712063+braindetrain@users.noreply.github.com>
2024-03-16 16:16:51 -0400

    Create Main.java

---
+++ IndexMakerMap_Lab/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    try{
+      IndexMakerMap.main(args);
+    }
+    catch(Exception e){
+      System.out.println("Something went wrong");
+    }
+  }
+}
